7d3bfbdb472b1ca03b913f669f70fcc66ea1f2e6
Forrest Tindall <144488579+forresttindall@users.noreply.github.com>
2024-11-02 14:36:18 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -13,14 +13,66 @@ form.addEventListener('submit', (e) => {
 
 
     let lifeExpectancy;
-    if (gender === 'male' && smoker) {
-        lifeExpectancy = 60.2;
-    } else if (gender === 'male' && !smoker) {
-        lifeExpectancy = 73.2;
-    } else if (gender === 'female' && smoker) {
-        lifeExpectancy = 66.1;
-    } else if (gender === 'female' && !smoker) {
-        lifeExpectancy = 79.1;
+    if (country === 'US') {
+        if (gender === 'male' && smoker) {
+            lifeExpectancy = 60.2;
+        } else if (gender === 'male' && !smoker) {
+            lifeExpectancy = 73.2;
+        } else if (gender === 'female' && smoker) {
+            lifeExpectancy = 66.1;
+        } else if (gender === 'female' && !smoker) {
+            lifeExpectancy = 79.1;
+        }
+    } else if (country === 'Canada') {
+        if (gender === 'male' && smoker) {
+            lifeExpectancy = 77;
+        } else if (gender === 'male' && !smoker) {
+            lifeExpectancy = 81.9;
+        } else if (gender === 'female' && smoker) {
+            lifeExpectancy = 80;
+        } else if (gender === 'female' && !smoker) {
+            lifeExpectancy = 86;
+        }
+    } else if (country === 'Uk') {
+        if (gender === 'male' && smoker) {
+            lifeExpectancy = 76;
+        } else if (gender === 'male' && !smoker) {
+            lifeExpectancy = 80;
+        } else if (gender === 'female' && smoker) {
+            lifeExpectancy = 80;
+        } else if (gender === 'female' && !smoker) {
+            lifeExpectancy = 84;
+        }
+    } else if (country === 'Japan') {
+        if (gender === 'male' && smoker) {
+            lifeExpectancy = 78;
+        } else if (gender === 'male' && !smoker) {
+            lifeExpectancy = 81.6;
+        } else if (gender === 'female' && smoker) {
+            lifeExpectancy = 84;
+        } else if (gender === 'female' && !smoker) {
+            lifeExpectancy = 87.7;
+        }
+    } else if (country === 'China') {
+        if (gender === 'male' && smoker) {
+            lifeExpectancy = 73;
+        } else if (gender === 'male' && !smoker) {
+            lifeExpectancy = 76.5;
+        } else if (gender === 'female' && smoker) {
+            lifeExpectancy = 77;
+        } else if (gender === 'female' && !smoker) {
+            lifeExpectancy = 80.5;
+        }
+    } else if (country === 'India') {
+        if (gender === 'male' && smoker) {
+            lifeExpectancy = 66;
+        } else if (gender === 'male' && !smoker) {
+            lifeExpectancy = 69.5;
+        } else if (gender === 'female' && smoker) {
+            lifeExpectancy = 69;
+        } else if (gender === 'female' && !smoker) {
+            lifeExpectancy = 72.5;
+        }
     }
 
     const lifeLeft = lifeExpectancy - age;
